0b6aad06e981fa57ed91203bdfeeb9467d896297
Lucca Proença souza <luccaproenca@gmail.com>
2022-01-05 16:03:32 -0300

    sem imagens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import { FiActivity } from "react-icons/fi";
-import './src/imag';
+import React from "react";
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
 function App() {
   return (
