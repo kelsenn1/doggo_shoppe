149194ab4c4a52903ec59940618273d87d79d198
Lucca Proença souza <luccaproenca@gmail.com>
2022-01-11 13:01:17 -0300

    seguindo o vid

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
                                     if (searchTerm == "") {
                                         return val }
                                     else if (val.name.toLowerCase().includes(searchTerm.toLowerCase)) {
-                                        return val;
+                                        return val
                                     }
                                     }
                                 ).map((val, key) => {
